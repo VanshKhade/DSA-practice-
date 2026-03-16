dfe26af13518d831d58c70c7d099034373aa597b
Kaizen <vanshkhade2005@gmail.com>
2026-03-16 18:34:56 +0530

    Add reversing stack using recursion

---
+++ c++/Stack/ReverseStack_recursion.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void insertAtBottom(stack<int>& s, int x){
+
+    if(s.empty()){
+        s.push(x);
+        return;
+    }
+
+    int num = s.top();
+    s.pop();
+
+    insertAtBottom(s, x);
+
+    s.push(num);
+}
+
+void reverseStack(stack<int>& s){
+
+    if(s.empty()){
+        return;
+    }
+
+    int num = s.top();
+    s.pop();
+
+    reverseStack(s);
+
+    insertAtBottom(s, num);
+}
+
+int main(){
+
+    stack<int> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    reverseStack(s);
+
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+}
